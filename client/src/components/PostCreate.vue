b68e29d613c8aa36b306f981379201292b4b566a
Lywën <laurie@alarcon.eu>
2023-01-03 12:27:42 +0100

    creation postcreate.vue

---
+++ client/src/components/PostCreate.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div class="createPost">Create Post</div>
+  <div>
+    <form action="" method="post">
+      <label for="titre">Titre: </label>
+      <input type="text" name="titre" id="titre" required />
+      <input type="submit" value="Envoyer" />
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.createPost {
+  color: red;
+}
+</style>
