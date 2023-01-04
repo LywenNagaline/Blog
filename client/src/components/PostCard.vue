3461b1ed1522872c72aaf6fba6731cd4b0016af3
Lywën <laurie@alarcon.eu>
2023-01-04 16:41:56 +0100

    ajout postCard

---
+++ client/src/components/PostCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h3>{{ currentPost.title }}</h3>
+      <!-- On transmets les props aux vues des comments pour qu'ils puissent avoir l'id des posts auquel se rattacher-->
+      <CommentList :postId="currentPost.id"></CommentList>
+      <CommentCreate :postId="currentPost.id"></CommentCreate>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import CommentCreate from "./CommentCreate.vue";
+import CommentList from "./CommentList.vue";
+
+//Creation de la props pour transmettre les données du post à PostList
+defineProps({
+  currentPost: Object,
+});
+</script>
+
+<style></style>
