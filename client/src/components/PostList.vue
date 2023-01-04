694cfd5b6e644467ed2046df05ebe42ebb25be1f
Lywën <laurie@alarcon.eu>
2023-01-04 16:47:37 +0100

    modif PostCard

--- client/src/components/PostList.vue
+++ client/src/components/PostList.vue
@@ -1,34 +1,36 @@
 <template>
   <div class="d-flex flex-row flex-wrap justify-content-between">
-    <div class="card" v-for="post in sortedPosts" :key="post.id">
-      <div class="card-body">
-        <h3>{{ post.title }}</h3>
-        <!-- On trnasmets les props aux vues des comments pour qu'ils puissent avoir l'id des posts auquel se rattacher-->
-        <CommentList :postId="post.id"></CommentList>
-        <CommentCreate :postId="post.id"></CommentCreate>
-      </div>
-    </div>
+    <PostCard
+      v-for="post of sortedPosts"
+      :key="post.id"
+      :currentPost="post"
+    ></PostCard>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import CommentCreate from "./CommentCreate.vue";
-import CommentList from "./CommentList.vue";
+import axios from "axios";
+import { onMounted, reactive, computed } from "vue";
+import PostCard from "./PostCard.vue";
 
-//On transmet la données à APP
-const propsPost = defineProps({
-  post: Object,
+const state = reactive({
+  posts: {},
 });
 
-//On transforme les objets d'objets en tableau d'objets
+async function fetchPosts() {
+  const response = await axios.get("http://localhost:4001/posts");
+  state.posts = response.data;
+}
+
 const sortedPosts = computed(() => {
   const transformedPosts = [];
-  for (const post of Object.values(propsPost.post)) {
+  for (const post of Object.values(state.posts)) {
     transformedPosts.push(post);
   }
   return transformedPosts;
 });
-</script>
 
-<style></style>
+onMounted(() => {
+  fetchPosts();
+});
+</script>
